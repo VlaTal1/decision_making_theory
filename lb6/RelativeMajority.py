e4657fc0aed79e40a3c337acf3a88a70a63650f2
VladyslavTalakh <vladiktalah@gmail.com>
2024-05-04 12:23:03 +0300

    Updated RelativeMajority

--- lb6/RelativeMajority.py
+++ lb6/RelativeMajority.py
@@ -1,22 +1,19 @@
-from utils import sort_scores_desc, init_alternatives_scores
+from utils import sort_scores_desc, init_alternatives_scores, execute_round
 from pprint import pprint as pp
 
 
 class RelativeMajority:
     def __init__(self, voting_profile: dict[tuple, int]):
         self.voting_profile = voting_profile
-        self.alternatives_scores = init_alternatives_scores(list(voting_profile.keys())[0])
+        self.alternatives_scores = {}
         self.winner = None
 
         self.find_winner()
 
     def find_winner(self):
-        for vote, amount in self.voting_profile.items():
-            alt = vote[0]
-            self.alternatives_scores[alt] += amount
+        scores, winner = execute_round(self.voting_profile)
+        self.alternatives_scores = scores
+        self.winner = winner
 
-        sorted_scores = sort_scores_desc(self.alternatives_scores)
-        self.winner = sorted_scores[0][0]
-
-        pp(self.alternatives_scores)
+        print(self.alternatives_scores)
         print(self.winner)
--- lb6/utils.py
+++ lb6/utils.py
@@ -7,3 +7,15 @@ def init_alternatives_scores(vote: tuple):
     for value in vote:
         alts[value] = 0
     return alts
+
+
+def execute_round(voting_profile: dict[tuple, int]):
+    scores = init_alternatives_scores(list(voting_profile.keys())[0])
+    for vote, amount in voting_profile.items():
+        alt = vote[0]
+        scores[alt] += amount
+
+    sorted_scores = sort_scores_desc(scores)
+    winner = sorted_scores[0][0]
+
+    return scores, winner
